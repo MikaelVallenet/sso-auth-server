02417956f0d6bb2007d4a4617c84015a5c6ce599
Mikatech <mikael.vallenet@epitech.eu>
2023-05-04 23:55:41 +0200

    feat: finish data validation

--- go/pkg/routes/routes.pb.go
+++ go/pkg/routes/routes.pb.go
@@ -26,8 +26,8 @@ const _ = proto.GoGoProtoPackageIsVersion3 // please upgrade the proto package
 
 type RegisterInput struct {
 	Username string `protobuf:"bytes,1,opt,name=username,proto3" json:"username,omitempty" json:"username" form:"username" binding:"required,min=3,max=20"`
-	Email    string `protobuf:"bytes,2,opt,name=email,proto3" json:"email,omitempty"`
-	Password string `protobuf:"bytes,3,opt,name=password,proto3" json:"password,omitempty"`
+	Email    string `protobuf:"bytes,2,opt,name=email,proto3" json:"email,omitempty" json:"email" form:"email" binding:"required,email"`
+	Password string `protobuf:"bytes,3,opt,name=password,proto3" json:"password,omitempty" json:"password" form:"password" binding:"required,min=8,max=20"`
 }
 
 func (m *RegisterInput) Reset()         { *m = RegisterInput{} }
@@ -84,76 +84,32 @@ func (m *RegisterInput) GetPassword() string {
 	return ""
 }
 
-type RegisterOutput struct {
-	Res string `protobuf:"bytes,1,opt,name=res,proto3" json:"res,omitempty"`
-}
-
-func (m *RegisterOutput) Reset()         { *m = RegisterOutput{} }
-func (m *RegisterOutput) String() string { return proto.CompactTextString(m) }
-func (*RegisterOutput) ProtoMessage()    {}
-func (*RegisterOutput) Descriptor() ([]byte, []int) {
-	return fileDescriptor_c2350d19bac5b124, []int{1}
-}
-func (m *RegisterOutput) XXX_Unmarshal(b []byte) error {
-	return m.Unmarshal(b)
-}
-func (m *RegisterOutput) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
-	if deterministic {
-		return xxx_messageInfo_RegisterOutput.Marshal(b, m, deterministic)
-	} else {
-		b = b[:cap(b)]
-		n, err := m.MarshalToSizedBuffer(b)
-		if err != nil {
-			return nil, err
-		}
-		return b[:n], nil
-	}
-}
-func (m *RegisterOutput) XXX_Merge(src proto.Message) {
-	xxx_messageInfo_RegisterOutput.Merge(m, src)
-}
-func (m *RegisterOutput) XXX_Size() int {
-	return m.Size()
-}
-func (m *RegisterOutput) XXX_DiscardUnknown() {
-	xxx_messageInfo_RegisterOutput.DiscardUnknown(m)
-}
-
-var xxx_messageInfo_RegisterOutput proto.InternalMessageInfo
-
-func (m *RegisterOutput) GetRes() string {
-	if m != nil {
-		return m.Res
-	}
-	return ""
-}
-
 func init() {
 	proto.RegisterType((*RegisterInput)(nil), "sso.routes.RegisterInput")
-	proto.RegisterType((*RegisterOutput)(nil), "sso.routes.RegisterOutput")
 }
 
 func init() { proto.RegisterFile("protos/routes.proto", fileDescriptor_c2350d19bac5b124) }
 
 var fileDescriptor_c2350d19bac5b124 = []byte{
-	// 270 bytes of a gzipped FileDescriptorProto
+	// 275 bytes of a gzipped FileDescriptorProto
 	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0xe2, 0x12, 0x2e, 0x28, 0xca, 0x2f,
 	0xc9, 0x2f, 0xd6, 0x2f, 0xca, 0x2f, 0x2d, 0x49, 0x2d, 0xd6, 0x03, 0xf3, 0x84, 0xb8, 0x8a, 0x8b,
 	0xf3, 0xf5, 0x20, 0x22, 0x52, 0xba, 0xe9, 0x99, 0x25, 0x19, 0xa5, 0x49, 0x7a, 0xc9, 0xf9, 0xb9,
 	0xfa, 0xe9, 0xf9, 0xe9, 0xf9, 0xfa, 0x60, 0x25, 0x49, 0xa5, 0x69, 0x60, 0x1e, 0x98, 0x03, 0x66,
-	0x41, 0xb4, 0x2a, 0x2d, 0x62, 0xe4, 0xe2, 0x0d, 0x4a, 0x4d, 0xcf, 0x2c, 0x2e, 0x49, 0x2d, 0xf2,
+	0x41, 0xb4, 0x2a, 0xcd, 0x67, 0xe2, 0xe2, 0x0d, 0x4a, 0x4d, 0xcf, 0x2c, 0x2e, 0x49, 0x2d, 0xf2,
 	0xcc, 0x2b, 0x28, 0x2d, 0x11, 0x8a, 0xe7, 0xe2, 0x28, 0x2d, 0x4e, 0x2d, 0xca, 0x4b, 0xcc, 0x4d,
 	0x95, 0x60, 0x54, 0x60, 0xd4, 0xe0, 0x74, 0x72, 0xfe, 0x74, 0x4f, 0xde, 0x3e, 0xab, 0x38, 0x3f,
 	0xcf, 0x4a, 0x09, 0x26, 0xa3, 0xa4, 0x90, 0x96, 0x5f, 0x94, 0x8b, 0xcc, 0x4f, 0xca, 0xcc, 0x4b,
 	0xc9, 0xcc, 0x4b, 0xb7, 0x52, 0x2a, 0x4a, 0x2d, 0x2c, 0xcd, 0x2c, 0x4a, 0x4d, 0xd1, 0xc9, 0xcd,
-	0xcc, 0xb3, 0x35, 0xd6, 0xc9, 0x4d, 0xac, 0xb0, 0x35, 0x32, 0x50, 0x0a, 0x82, 0x1b, 0x2a, 0x24,
-	0xc2, 0xc5, 0x9a, 0x9a, 0x9b, 0x98, 0x99, 0x23, 0xc1, 0x04, 0x32, 0x3d, 0x08, 0xc2, 0x11, 0x92,
-	0xe2, 0xe2, 0x28, 0x48, 0x2c, 0x2e, 0x2e, 0xcf, 0x2f, 0x4a, 0x91, 0x60, 0x06, 0x4b, 0xc0, 0xf9,
-	0x4a, 0x4a, 0x5c, 0x7c, 0x30, 0x37, 0xfa, 0x97, 0x96, 0x80, 0x1c, 0x29, 0xc0, 0xc5, 0x5c, 0x94,
-	0x5a, 0x0c, 0x71, 0x5f, 0x10, 0x88, 0xe9, 0xa4, 0x7e, 0xe2, 0x91, 0x1c, 0xe3, 0x85, 0x47, 0x72,
-	0x8c, 0x0f, 0x1e, 0xc9, 0x31, 0x4e, 0x78, 0x2c, 0xc7, 0x70, 0xe1, 0xb1, 0x1c, 0xc3, 0x8d, 0xc7,
-	0x72, 0x0c, 0x51, 0xbc, 0xa0, 0x40, 0xc8, 0x4e, 0x87, 0x06, 0x59, 0x12, 0x1b, 0xd8, 0xe3, 0xc6,
-	0x80, 0x00, 0x00, 0x00, 0xff, 0xff, 0x95, 0x57, 0x69, 0x0d, 0x4a, 0x01, 0x00, 0x00,
+	0xcc, 0xb3, 0x35, 0xd6, 0xc9, 0x4d, 0xac, 0xb0, 0x35, 0x32, 0x50, 0x0a, 0x82, 0x1b, 0x2a, 0xe4,
+	0xc3, 0xc5, 0x9a, 0x9a, 0x9b, 0x98, 0x99, 0x23, 0xc1, 0x04, 0x36, 0xdd, 0xec, 0xd3, 0x3d, 0x79,
+	0x23, 0x88, 0xe9, 0x60, 0x61, 0x98, 0xd1, 0x50, 0x0e, 0xa6, 0xb9, 0x10, 0x89, 0x20, 0x88, 0x21,
+	0x20, 0xe7, 0x16, 0x24, 0x16, 0x17, 0x97, 0xe7, 0x17, 0xa5, 0x48, 0x30, 0xa3, 0x3b, 0x17, 0x26,
+	0x03, 0x33, 0x13, 0xc1, 0xc7, 0xee, 0x5c, 0x0b, 0x84, 0x73, 0x61, 0x4a, 0x9d, 0xd4, 0x4f, 0x3c,
+	0x92, 0x63, 0xbc, 0xf0, 0x48, 0x8e, 0xf1, 0xc1, 0x23, 0x39, 0xc6, 0x09, 0x8f, 0xe5, 0x18, 0x2e,
+	0x3c, 0x96, 0x63, 0xb8, 0xf1, 0x58, 0x8e, 0x21, 0x8a, 0x17, 0x14, 0xba, 0xd9, 0xe9, 0xd0, 0xb8,
+	0x48, 0x62, 0x03, 0x87, 0xa8, 0x31, 0x20, 0x00, 0x00, 0xff, 0xff, 0x9a, 0xc1, 0x56, 0x1c, 0xa3,
+	0x01, 0x00, 0x00,
 }
 
 func (m *RegisterInput) Marshal() (dAtA []byte, err error) {
@@ -200,36 +156,6 @@ func (m *RegisterInput) MarshalToSizedBuffer(dAtA []byte) (int, error) {
 	return len(dAtA) - i, nil
 }
 
-func (m *RegisterOutput) Marshal() (dAtA []byte, err error) {
-	size := m.Size()
-	dAtA = make([]byte, size)
-	n, err := m.MarshalToSizedBuffer(dAtA[:size])
-	if err != nil {
-		return nil, err
-	}
-	return dAtA[:n], nil
-}
-
-func (m *RegisterOutput) MarshalTo(dAtA []byte) (int, error) {
-	size := m.Size()
-	return m.MarshalToSizedBuffer(dAtA[:size])
-}
-
-func (m *RegisterOutput) MarshalToSizedBuffer(dAtA []byte) (int, error) {
-	i := len(dAtA)
-	_ = i
-	var l int
-	_ = l
-	if len(m.Res) > 0 {
-		i -= len(m.Res)
-		copy(dAtA[i:], m.Res)
-		i = encodeVarintRoutes(dAtA, i, uint64(len(m.Res)))
-		i--
-		dAtA[i] = 0xa
-	}
-	return len(dAtA) - i, nil
-}
-
 func encodeVarintRoutes(dAtA []byte, offset int, v uint64) int {
 	offset -= sovRoutes(v)
 	base := offset
@@ -256,19 +182,6 @@ func (m *RegisterInput) Size() (n int) {
 		n += 1 + l + sovRoutes(uint64(l))
 	}
 	l = len(m.Password)
-	if l > 0 {
-		n += 1 + l + sovRoutes(uint64(l))
-	}
-	return n
-}
-
-func (m *RegisterOutput) Size() (n int) {
-	if m == nil {
-		return 0
-	}
-	var l int
-	_ = l
-	l = len(m.Res)
 	if l > 0 {
 		n += 1 + l + sovRoutes(uint64(l))
 	}
@@ -405,88 +318,6 @@ func (m *RegisterInput) Unmarshal(dAtA []byte) error {
 				return io.ErrUnexpectedEOF
 			}
 			m.Password = string(dAtA[iNdEx:postIndex])
-			iNdEx = postIndex
-		default:
-			iNdEx = preIndex
-			skippy, err := skipRoutes(dAtA[iNdEx:])
-			if err != nil {
-				return err
-			}
-			if (skippy < 0) || (iNdEx+skippy) < 0 {
-				return ErrInvalidLengthRoutes
-			}
-			if (iNdEx + skippy) > l {
-				return io.ErrUnexpectedEOF
-			}
-			iNdEx += skippy
-		}
-	}
-
-	if iNdEx > l {
-		return io.ErrUnexpectedEOF
-	}
-	return nil
-}
-func (m *RegisterOutput) Unmarshal(dAtA []byte) error {
-	l := len(dAtA)
-	iNdEx := 0
-	for iNdEx < l {
-		preIndex := iNdEx
-		var wire uint64
-		for shift := uint(0); ; shift += 7 {
-			if shift >= 64 {
-				return ErrIntOverflowRoutes
-			}
-			if iNdEx >= l {
-				return io.ErrUnexpectedEOF
-			}
-			b := dAtA[iNdEx]
-			iNdEx++
-			wire |= uint64(b&0x7F) << shift
-			if b < 0x80 {
-				break
-			}
-		}
-		fieldNum := int32(wire >> 3)
-		wireType := int(wire & 0x7)
-		if wireType == 4 {
-			return fmt.Errorf("proto: RegisterOutput: wiretype end group for non-group")
-		}
-		if fieldNum <= 0 {
-			return fmt.Errorf("proto: RegisterOutput: illegal tag %d (wire type %d)", fieldNum, wire)
-		}
-		switch fieldNum {
-		case 1:
-			if wireType != 2 {
-				return fmt.Errorf("proto: wrong wireType = %d for field Res", wireType)
-			}
-			var stringLen uint64
-			for shift := uint(0); ; shift += 7 {
-				if shift >= 64 {
-					return ErrIntOverflowRoutes
-				}
-				if iNdEx >= l {
-					return io.ErrUnexpectedEOF
-				}
-				b := dAtA[iNdEx]
-				iNdEx++
-				stringLen |= uint64(b&0x7F) << shift
-				if b < 0x80 {
-					break
-				}
-			}
-			intStringLen := int(stringLen)
-			if intStringLen < 0 {
-				return ErrInvalidLengthRoutes
-			}
-			postIndex := iNdEx + intStringLen
-			if postIndex < 0 {
-				return ErrInvalidLengthRoutes
-			}
-			if postIndex > l {
-				return io.ErrUnexpectedEOF
-			}
-			m.Res = string(dAtA[iNdEx:postIndex])
 			iNdEx = postIndex
 		default:
 			iNdEx = preIndex
