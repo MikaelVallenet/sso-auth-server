a4c5fc9caefc091cf17965685e021d23ba6dfed7
Mikatech <mikael.vallenet@epitech.eu>
2023-05-04 17:46:42 +0200

    feat: add login & token route

--- go/pkg/routes/router.go
+++ go/pkg/routes/router.go
@@ -5,5 +5,7 @@ import "github.com/gin-gonic/gin"
 func Init() *gin.Engine {
 	router := gin.Default()
 	router.POST("/register", Register)
+	router.POST("/login", Login)
+	router.POST("/token", Token)
 	return router
 }
